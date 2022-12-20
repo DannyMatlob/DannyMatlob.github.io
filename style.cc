8bf9620a4527b0266857109101a4818b7fc00a33
Danny Matlob <44306495+DannyMatlob@users.noreply.github.com>
2022-12-19 20:44:31 -0800

    Create style.cc

---
+++ style.cc
@@ -0,0 +1,15 @@
+body {
+  font-family: Arial, sans-serif;
+  color: #333;
+  background-color: #fff;
+}
+
+h1 {
+  color: #00b8d4;
+  text-align: center;
+}
+
+a {
+  color: #00b8d4;
+  text-decoration: none;
+}
