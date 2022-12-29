6e61392eb6652dc2c54ddcdf8a6e36caa7a29db1
Danny Matlob <44306495+DannyMatlob@users.noreply.github.com>
2022-12-28 22:29:26 -0800

    Update descriptions.js

--- descriptions.js
+++ descriptions.js
@@ -1,8 +1,11 @@
 let activeIndex = 0;
+const projectClass = ["about", "proj1", "proj2", "proj3", "proj4", "proj5", "proj6"];
 
 descriptions = document.getElementsByClassName("project");
+links = document.querySelectorAll("#project-buttons a");
+console.log(links);
 
-for (let i = 1; i <= 4; i++) {
+for (let i = 1; i <= descriptions.length; i++) {
     document.getElementById("link" + i).addEventListener("click", function() {
         let index = i;
         console.log("setting", index);
@@ -17,5 +20,13 @@ for (let i = 1; i <= 4; i++) {
 function updateMain (index) {
     descriptions[index].setAttribute("id", "active");
     descriptions[activeIndex].setAttribute("id", "hidden");
+
+    console.log(projectClass[index]);
+
+    document.getElementById("projectBox").className = projectClass[index];
+    links.forEach(element => {
+        element.className = projectClass[index];
+    });
+
     activeIndex = index;
 }
